10a641254654bcae8465ab5abe4e58840f3d4518
Fenar7 <fenarkhan@gmail.com>
2026-02-28 02:12:57 +0530

    sdfsdf

---
+++ app/components/OurApproachSection/OurApproachSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const OurApproachSection = () => {
+  return (
+    <div className="ourapparoach-section-container flex items-center justify-center">
+<div className="ourapproach-section container">
+    #title section goes ehre
+
+    <div className="feature-items-container">
+            <div className="feature-item">
+                <p>number here</p>
+                <img src="" alt="" />
+                <p>subtext here</p>
+            </div>
+
+            #border in between
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default OurApproachSection
