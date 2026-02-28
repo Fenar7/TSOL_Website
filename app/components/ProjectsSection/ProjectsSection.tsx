68127daddeebb27a0fd879b6c8656177b76f5652
Fenar7 <fenarkhan@gmail.com>
2026-02-28 14:42:05 +0530

    proejcts

---
+++ app/components/ProjectsSection/ProjectsSection.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectsSection = () => {
+  return 
+    <div className='projects-section-container container flex items-center justify-center'>
+<div className="projects-section-container container">
+#title goes here
+
+<div className="project-cards-container">
+    <div className="project-item">
+        <img src="" alt="" />
+        <h4></h4>
+    </div>
+</div>
+</div>
+    </div>
+  
+}
+
+export default ProjectsSection
