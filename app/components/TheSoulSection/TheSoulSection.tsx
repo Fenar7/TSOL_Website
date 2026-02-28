e1e1dc9567b40c2427288189cfc62731c4d880fb
Fenar7 <fenarkhan@gmail.com>
2026-02-28 09:54:59 +0530

    sdfsdfsdfsdf

---
+++ app/components/TheSoulSection/TheSoulSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TheSoulSection = () => {
+  return (
+    <div className="the-soul-conatiner-main flex items-center justify-center">
+
+    </div>
+  )
+}
+
+export default TheSoulSection
