daff042c185ef8bf3a0c0d85c0bfba717232ba3d
Fenar7 <fenarkhan@gmail.com>
2026-02-28 20:27:16 +0530

    dsfsdf

---
+++ app/components/ContactCtaSection/ContactCtaSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactCtaSection = () => {
+  return (
+    <div className='contact-cta-section-container flex items-center justify-center'>
+<div className="contact-cta-section container">
+    
+</div>
+    </div>
+  )
+}
+
+export default ContactCtaSection
